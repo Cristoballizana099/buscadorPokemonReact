be920950e9fc5477b535a6f5876ecbca5c382bd8
Cristoballizana099 <Cristoballizana91@gmail.com>
2023-10-25 22:45:36 -0600

    mejoras de estilos

--- src/componets/Search.jsx
+++ src/componets/Search.jsx
@@ -17,33 +17,46 @@ const pokemonDatos = {
   nombre: datosPokemon.name,
   numero: datosPokemon.id,
 
+
 }
 
 const onClick = async (event) =>{
   const data = await ApiPokemon(nombrePokemon)
   SetdatosPokemon(data)
+
+  
 }
 
 
 
   return (
+    
         <>
-        <section className='contenedoruno' >
-        <div >
-          <input type="text" onChange={Search} placeholder='Coloca Un Pokemon' />
-        </div>
-        <div>
-          <button onClick={onClick}>buscar Pokemon</button>
-        </div>
-        </section>
+
+              <section className='contenedoruno' >
+                    <div >
+                         <input type="text" onChange={Search} placeholder='Coloca Un Pokemon' />
+                    </div>
+                    <div>
+                         <button onClick={onClick}>buscar Pokemon</button>
+                    </div>
+              </section>
         
-        <section className='contenedordos'>
-          <div className='contenedorPokemon'>
-          <img className='imgPokemon' src={pokemonDatos.img}  />
-            <h1 className='DatosPokemon'>nombre : {pokemonDatos.nombre}</h1>
-            <p >numero Pokedex : {pokemonDatos.numero}</p>
-          </div>
-        </section>
+
+
+              <section className='contenedordos'>
+                    <div className='contenedorPokemon'>
+                        <div>
+                        <img className='imgPokemon' src={pokemonDatos.img}  />
+                        </div>
+                        <div className='DatosPokemon'>
+                        <h1 className='nombrePokemon'>{pokemonDatos.nombre}</h1>
+                                <p >N Pokedex : {pokemonDatos.numero}</p>
+                                
+                        </div>
+                         
+                    </div>
+              </section>
         </>
   )
 }
